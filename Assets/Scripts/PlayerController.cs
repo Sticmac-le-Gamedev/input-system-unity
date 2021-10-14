c73de4179f24853344be25a8f478713924324bb5
Julien Lemaire <julien.lemaire@sticmac.fr>
2021-10-15 00:34:28 +0200

    Saut du personnage

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,19 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] PlayerMovement _playerMovement;
 
+    private bool _isJumping = false;
     void Update() {
         _playerMovement.Move(Math.Sign(Input.GetAxis("Horizontal"))); // On ne convertit qu'au dernier moment pour limiter le nombre de casts
+
+        if (Input.GetButtonDown("Jump")) {
+            _isJumping = true;
+        }
+    }
+
+    void FixedUpdate() {
+        if (_isJumping) {
+            _playerMovement.Jump();
+            _isJumping = false;
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] SpriteRenderer _renderer;
     [SerializeField] Rigidbody2D _rb2d;
     [SerializeField] Animator _animator;
 
+    [Header("Ground Check")]
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] LayerMask _groundLayer;
+
+    [Header("Parameters")]
     [SerializeField] float _speed;
+    [SerializeField] float _jumpForce = 200f;
     [SerializeField] float _movementSmoothing = 0.05f;
 
     private Vector2 _targetVelocity = Vector2.zero;
+    private bool _grounded = true;
 
     void FixedUpdate() {
         Vector2 v = new Vector2();
         _targetVelocity = new Vector2(_targetVelocity.x, _rb2d.velocity.y); // On préserve la vélocité verticale
         _rb2d.velocity = Vector2.SmoothDamp(_rb2d.velocity, _targetVelocity, ref v, _movementSmoothing);
+
+        // Vérifie que l'on touche le sol en tombant
+        if (!_grounded && _rb2d.velocity.y < -0.001f && Physics2D.OverlapCircle(_groundCheck.position, 0.2f, _groundLayer)) {
+            _animator.SetBool("Jumping", false);
+            _grounded = true;
+        }
     }
 
     public void Move(int direction) {
@@ -33,5 +47,13 @@ public class PlayerMovement : MonoBehaviour
 
     public void Flip() {
         _renderer.flipX = !_renderer.flipX;
+    }
+
+    public void Jump() {
+        if (_grounded) { // Saut autorisé uniquement si on se trouve au sol
+            _grounded = false;
+            _animator.SetBool("Jumping", true);
+            _rb2d.AddForce(new Vector2(0f, _jumpForce)); // Force vers le haut pour le saut
+        }
     }
 }
